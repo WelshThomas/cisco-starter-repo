d041b208ee472c82dec7acd42b5079c711605675
ThomasWoodthorpe <32986093+WelshThomas@users.noreply.github.com>
2023-07-10 21:06:39 +0100

    Task 3

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -8,13 +8,19 @@ function App() {
         <h1>Sextant</h1>
       </div>
 
-      <ExhibitComponent title="IPV4 Address" content={
-        <DataComp url="https://api.ipify.org?format=json"/>
-      }/>
+      <div className="Component_Grid">
+        <ExhibitComponent title="IPV4 Address" content={
+          <DataComp url="https://api.ipify.org?format=json"/>
+        }/>
 
-      <ExhibitComponent title="IPV6 Address" content={
-        <DataComp url="https://api64.ipify.org?format=json"/>
-      }/>
+        <ExhibitComponent title="IPV6 Address" content={
+          <DataComp url="https://api64.ipify.org?format=json"/>
+        }/>
+
+        <ExhibitComponent title="Latency Websocket" content={
+          <WebSocketComp />
+        }/>
+      </div>
     </div>
   );
 }
@@ -52,6 +58,41 @@ function DataComp(props){
       {data ? <p>{data.ip}</p> : <p>Loading...</p>}
     </div>
   )
+}
+
+function WebSocketComp(){
+  var lastEpoch = null;
+  var [data, setData] = useState(null);
+  React.useEffect(() => {
+    const websocket = new WebSocket("ws://localhost:55455")
+
+    websocket.onopen =() =>{
+      console.log("Connected");
+    }
+
+    websocket.onmessage = (event) => {
+      var dataFound = JSON.parse(event.data);
+      var data = 0;
+      console.log(dataFound);
+      if(lastEpoch != null){
+        data = dataFound - lastEpoch;
+      }
+
+      lastEpoch = dataFound;
+
+      data = data/10;
+
+      setData(data);
+    }
+
+    return () => {
+      websocket.close();
+    }
+  }, [])
+
+    return (      
+      <p>{data}ms</p>
+    )
 }
 
 export default App;
