f6e10295f0284bce8b646866699bb3ed2333fb8e
ThomasWoodthorpe <32986093+WelshThomas@users.noreply.github.com>
2023-07-10 20:08:22 +0100

    Task 3

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
   return (
@@ -7,8 +8,12 @@ function App() {
         <h1>Sextant</h1>
       </div>
 
-      <ExhibitComponent title="Title Here" content={
-        <p>Content is here<p>Second content element is here</p></p>
+      <ExhibitComponent title="IPV4 Address" content={
+        <DataComp url="https://api.ipify.org?format=json"/>
+      }/>
+
+      <ExhibitComponent title="IPV6 Address" content={
+        <DataComp url="https://api64.ipify.org?format=json"/>
       }/>
     </div>
   );
@@ -21,6 +26,30 @@ function ExhibitComponent(props) {
       <div className="Exhibit_Content">
         {props.content}
       </div>
+    </div>
+  )
+}
+
+
+function DataComp(props){
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData(){
+      try{
+        const response = await fetch(props.url);
+        const data = await response.json();
+        setData(data);
+      } catch (error){
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [props.url]);
+
+  return (
+    <div>
+      {data ? <p>{data.ip}</p> : <p>Loading...</p>}
     </div>
   )
 }
